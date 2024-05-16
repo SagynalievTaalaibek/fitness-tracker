2f9f07156709f22f226d74837996ab9e3d6024a1
SagynalievTaalaibek <sagynalievv.t@gmail.com>
2024-05-16 23:49:39 +0600

    api: add log in and log out functions

--- api-fitness/routes/userRouter.ts
+++ api-fitness/routes/userRouter.ts
@@ -26,6 +26,58 @@ userRouter.post('/', async (req, res, next) => {
   }
 });
 
+userRouter.post('/sessions', async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(422).send({ error: 'Email and password not correct!' });
+    }
+
+    const isMatch = await user.checkPassword(req.body.password);
+
+    if (!isMatch) {
+      return res.status(422).send({ error: 'Email and password not correct!' });
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send({ message: 'Email and password are correct!', user });
+  } catch (error) {
+    return next(error);
+  }
+});
+
+userRouter.delete('/sessions', async (req, res, next) => {
+  try {
+    const headerValue = req.get('Authorization');
+    const successMessage = { message: 'Success!!' };
+
+    if (!headerValue) {
+      return res.send(successMessage);
+    }
+
+    const [_bearer, token] = headerValue.split(' ');
+
+    if (!token) {
+      return res.send(successMessage);
+    }
+
+    const user = await User.findOne({ token });
+
+    if (!user) {
+      return res.send(successMessage);
+    }
+
+    user.generateToken();
+    await user.save();
+
+    return res.send(successMessage);
+  } catch (e) {
+    next(e);
+  }
+});
+
 userRouter.patch(
   '/:id',
   imageUpload.single('avatar'),
